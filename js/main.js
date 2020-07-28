bf530f0bf94e7caa264988622bf02454e56f56f9
cermak <pcermak@live.com>
2020-07-28 12:45:36 +0200

    Dates corrected

--- js/main.js
+++ js/main.js
@@ -295,7 +295,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("21 December 2019 9:56:00 GMT+01:00");			
+		var endTime = new Date("18 October 2020 19:00:00 GMT+02:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
