0ec0b2860387fb8d1ce6a38915281b3e251aa58e
Petr Čermák <petr.cermak@matfyz.cuni.cz>
2024-06-24 15:21:11 +0200

    school 2024

--- js/main.js
+++ js/main.js
@@ -295,7 +295,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("19 October 2022 18:00:00 GMT+02:00");			
+		var endTime = new Date("1 October 2024 18:00:00 GMT+02:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
